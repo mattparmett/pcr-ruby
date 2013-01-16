74070ca7cde9c5db5f9b4c2e0f09b22abe4e9aea
Greg Terrono <gterrono@seas.upenn.edu>
2013-01-16 05:13:16 -0500

    Rewrote PCR as a module with a client. Builds but does not work

--- lib/pcr-ruby.rb
+++ lib/pcr-ruby.rb
@@ -1,28 +1,34 @@
 require 'json'
 require 'open-uri'
+require 'classes/course'
+require 'classes/errors'
+require 'classes/section'
+require 'classes/coursehistory'
+require 'classes/review'
+require 'classes/string'
 
 #PCR class handles token and api url, so both are easily changed
-class PCR
-  attr_accessor :token, :api_endpt
+module PCR
+  class Client
+    def initialize(token, api_endpt = "http://api.penncoursereview.com/v1/")
+      @token = token
+      @api_endpt = api_endpt
+    end
   
-  def initialize(token, api_endpt = "http://api.penncoursereview.com/v1/")
-    @token = token
-    @api_endpt = api_endpt
-  end
-  
-  def coursehistory(course_code)
-    CourseHistory.new(course_code, self.api_endpt, self.token)
-  end
-  
-  # def instructor(id)
-    # Instructor.new(id)
-  # end
-  
-  def makeURL(path)
-    "#{self.api_endpt + path}?token=#{self.token}"
-  end
-  
-end
+    def get_json(path)
+      JSON.parse(open("#{@api_endpt + path}?token=#{@token}"))
+    end
 
-# Load classes
-Dir[File.dirname(__FILE__) + "/classes/*.rb"].each { |file| require file }
+    def coursehistory(course_code)
+      CourseHistory.new(course_code, self.api_endpt, self.token)
+    end
+
+    def instructor(id)
+      raise NotImplementedError.new("Instructors have not yet been implemented.")
+    end
+
+    def dept(code)
+      raise NotImplementedError.new("Departments have not yet been implemented.")
+    end
+  end
+end
